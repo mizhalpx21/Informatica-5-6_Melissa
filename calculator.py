57e2b6c3fd0db67cff644586ddd66a77945be403
Melissa <mizhalpx@gmail.com>
2025-08-15 22:54:33 +0000

    helhhhhh

--- calculator.py
+++ calculator.py
@@ -1,2 +1,7 @@
-x = input 
-y = input
+x = input("What is the value of x? ")
+y = input("What is the value of y? ")
+
+z= round(float(x) + float(y))
+
+#z:, is to add the comma to bigger numbers like 1000 
+print(f"{z:,}")
