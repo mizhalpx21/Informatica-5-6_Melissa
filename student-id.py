2a362a4e5e84e08b30c8c6df219db9aaae5d1a2f
Melissa <mizhalpx@gmail.com>
2025-08-20 20:20:10 +0000

    guanabana

---
+++ student-id.py
@@ -0,0 +1,19 @@
+def main():
+	print('')
+	print('Please enter the following information to create your ID Card.')
+	input('Press enter to continue.')
+	firstname = input("Type your first name: ")
+	lastname = input("Type your last name: ")
+	emailaddress = input("Type your email: ")
+	phonenumber = input("Type your phone number: ")
+	studentidnumber = input("Type your student ID number: ")
+	fptclass = input("Type your FPT class: ")
+	expectedgraduationyear = input("Type your graduation year: ")
+	favoritesubject = input("Type your favorite subject: ")
+	extracurricularactivities = input("Type Yes or Not if you have extracurricular activities: ")
+
+
+print("Your ID Card is: ")
+main()
+
+	
