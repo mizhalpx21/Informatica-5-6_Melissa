18d7203d9d3668ef0a0554bb4a469d4759cff2d7
Melissa <mizhalpx@gmail.com>
2025-08-21 20:20:03 +0000

    w

--- if-exercises.py
+++ if-exercises.py
@@ -1,10 +1,27 @@
+#def main():
+    #integer_number = int(input("Write an integer number: "))
+    #if integer_number < 0:
+        #print(integer_number * -1)
+    #if integer_number > 0:
+        #print(integer_number)
+
+#main()
+
+#def main():
+ #   x= int(input("Write a number: "))
+  #  y= int(input("Write a number: "))
+   # operation = input("Write the operation you want to do: ") 
+    #if operation == "*":
+     #   print(x * y)
+    #if operation == "+":
+    #    print(x + y)
+    #if operation == "-":
+    #    print(x - y)
+
+#main()
+
 def main():
-    integer_number = int(input("Write an integer number: "))
-    if integer_number < 0:
-        print(integer_number * -1)
-    if integer_number > 0:
-        print(integer_number)
+    arithmetic_expression = input("Write an arithmetic expression: ")
+    print(calculating )
 
 main()
-
-
