db2bec2eb1cfbdfcd69ac4fc9084b3c5c8b8ef5b
Melissa <mizhalpx@gmail.com>
2025-08-21 19:50:48 +0000

    jelly

---
+++ if-exercises.py
@@ -0,0 +1,10 @@
+def main():
+    integer_number = int(input("Write an integer number: "))
+    if integer_number < 0:
+        print(integer_number * -1)
+    if integer_number > 0:
+        print(integer_number)
+
+main()
+
+
