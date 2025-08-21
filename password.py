c309ecfb5d93726f4fa63fd2e34f58ee417a156a
Melissa <mizhalpx@gmail.com>
2025-08-21 16:40:44 +0000

    catandog

---
+++ password.py
@@ -0,0 +1,15 @@
+def main():
+    password = input("Set a password: ")
+    input("Press enter to long in.")
+    check_password(password)
+
+def check_password(password):
+    guess=input("Guess the password: ")
+    if password == guess:
+        print("You knew the password!")
+
+    if guess != password:
+        print("Get away from the keyboard you imposter!")
+
+main()
+    
