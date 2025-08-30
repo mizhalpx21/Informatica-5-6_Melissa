80a715b167888ae33d44c3907779405452095f9e
Melissa <mizhalpx@gmail.com>
2025-08-30 01:38:05 +0000

    th

--- Checkpoint 3/ life-loop.py
+++ Checkpoint 3/ life-loop.py
@@ -1,19 +1,17 @@
 def main():
-    daily = input("Hey! Is it morning or night where you are? ")
     option1 = "morning" 
     option2 = "night"
+    daily = input("Hey! Is it morning or night where you are? ")
 
-
-    if daily == option1:
-        print("A new day has started! Remember God loves you and wants you to talk to him❤️  SO DO NOT FORGET TO PRAY")
-
-    elif daily == option2:
-        print("The day has ended! God wants to now how was your day, SO DO NOT FORGET TO PRAY") 
-
-    else:
-        print("Okay, I hope you have a pretty good day") 
+    while daily:
     
-
+        if daily == option1:
+            print("A new day has started! Remember God loves you and wants you to talk to him❤️  SO DO NOT FORGET TO PRAY")
+        elif daily == option2:
+            print("The day has ended! God wants to now how was your day, SO DO NOT FORGET TO PRAY") 
+        else:
+            print("Okay, I hope you have a pretty good day") 
+        break
 
 main()
       
