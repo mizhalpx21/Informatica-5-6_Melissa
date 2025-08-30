8ee802cebeaf7334883df88c71c0186bc55ee09d
Melissa <mizhalpx@gmail.com>
2025-08-30 01:13:39 +0000

    r

--- Checkpoint 3/ life-loop.py
+++ Checkpoint 3/ life-loop.py
@@ -3,11 +3,17 @@ def main():
     option1 = "morning" 
     option2 = "night"
 
+
     if daily == option1:
-        print("A new day has started! Remember God loves you and wants you to talk to him ❤️ SO DO NOT FORGET TO PRAY ")
-    
+        print("A new day has started! Remember God loves you and wants you to talk to him❤️  SO DO NOT FORGET TO PRAY")
+
     elif daily == option2:
-        print("The day has ended! God wants to now how was your day, SO DO NOT FORGET TO PRAY")
+        print("The day has ended! God wants to now how was your day, SO DO NOT FORGET TO PRAY") 
+
+    else:
+        print("Okay, I hope you have a pretty good day") 
+    
+
 
 main()
       
---
+++ Checkpoint 3/lifeloop.py
@@ -0,0 +1,10 @@
+def main():
+
+    daily = input("Hey! Have you prayed today? Answer with a: Yes, I have or Not yet ")
+    option1 = "Yes, I have" 
+    option2 = "Not yet"
+
+while True:
+    print()
+
+    
