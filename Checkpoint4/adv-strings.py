ee45488e0da659616c9a5298347efb134bbd2135
Melissa <mizhalpx@gmail.com>
2025-09-01 19:39:52 +0000

    rfe

---
+++ Checkpoint4/adv-strings.py
@@ -0,0 +1,28 @@
+#String Lenght
+input_string = input("Please type a message: ")
+# print(input_string)
+
+# print("-" * len(input_string))
+
+# #String Index
+# print(input_string[0])
+# print(input_string[1])
+# print(input_string[-1])
+
+#Example
+# i = 0
+# while i < len(input_string):
+#     print(input_string[i])
+#     i += 1
+
+#hello
+# i = -1
+# while -i < len(input_string)-i:
+#     print(input_string[i])
+#     i -= 1 - 1 
+
+#True
+# print("h" in input_string)
+# print("j" in input_string)
+# print("ello" in input_string)
+# print(input_string.find("h"))
