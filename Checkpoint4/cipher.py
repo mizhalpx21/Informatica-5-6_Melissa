1b49278d0446c3822c4b84b4d81ed0398fcb8b24
Melissa <mizhalpx@gmail.com>
2025-09-02 16:40:05 +0000

    noo

---
+++ Checkpoint4/cipher.py
@@ -0,0 +1,18 @@
+def main():
+     message = input("Write a message: ").lower()
+     encode_message(message)
+
+
+def encode_message(text):
+    alphabet = "abcdefghijklmnopqrstuvwxyz"
+    cipher = "zyxwvutsrqponmlkjihgfedcba"
+    new_message = ""
+    i = 0
+
+    while i < len(text):
+         char = text[i]
+         
+
+
+   
+
