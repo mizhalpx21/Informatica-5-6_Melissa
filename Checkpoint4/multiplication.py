783e87eb4f8c7f8d58a68993a2481178920472c2
Melissa <mizhalpx@gmail.com>
2025-09-04 20:20:07 +0000

    er

--- Checkpoint4/multiplication.py
+++ Checkpoint4/multiplication.py
@@ -2,9 +2,18 @@ def main():
     positive_integer_number = int(input("Write a positive integer number from 1 to 10: "))
     multiplication( positive_integer_number)
 
-def multiplication( positive_integer_number)
-    if  positive_integer_number ==1 or positive_integer_number == 2 or positive_integer_number == 3 or positive_integer_number == 4 or positive_integer_number == 5 or positive_integer_number == 6 or positive_integer_number == 7 or positive_integer_number == 8 or positive_integer_number == 9 or positive_integer_number == 10:
+def multiplication(positive_integer_number):
+    n = 1
+    positive_integer_number == 1 or 2 or 3 or 4 or 5 or 6 or 7 or 8 or 9 or 10
+
+    while n<= 9:
+        n += 1
+        ans = positive_integer_number * n
+        print(f"{positive_integer_number} x {n} = {ans}")
+    if positive_integer_number != 
         
+main()
+       
         
 
     
