c357a09a418749eece53d1432695a8ea6eb2de3a
Melissa <mizhalpx@gmail.com>
2025-09-11 20:20:01 +0000

    trhhb

--- Checkpoint5/list-fun-prectice.py
+++ Checkpoint5/list-fun-prectice.py
@@ -1,12 +1,31 @@
 def main():
-    my_list = [2, 4, 6, 8, 10]
-    print(lenght(my_list))
+   print(lenght(values()))
+   mean(values())
+   range_list(values())
 
-def lenght(my_list):
-    return len(my_list)
+def values():
+    value_list = []
+    while True:
+        value = int(input("Enter a value: "))
+        if value != 0:
+            value_list.append(value)
+            print(value_list)
+            ordered_list = sorted(value_list)
+            print(ordered_list)
+            continue
+        else:
+            break
+    return value_list
 
-def mean(my_list):
-    
+def lenght(list):
+    return len(list)
 
+def mean(list):
+    print(sum(list) / len(list))
 
+def range_list(list):
+    print(max(list) - min(list))
+ 
 main()
+
+ 
