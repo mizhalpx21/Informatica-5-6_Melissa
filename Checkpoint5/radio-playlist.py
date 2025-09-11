8808c2d25183ab10898e1f469b571744b0d22188
Melissa <mizhalpx@gmail.com>
2025-09-11 01:47:05 +0000

    tr

---
+++ Checkpoint5/radio-playlist.py
@@ -0,0 +1,8 @@
+weekly_playlist = ["Blinding Lights", "Levitating", "As It Was", "Heat Waves", "Good 4 u"]
+
+weekly_playlist = []
+weekly_playlist.append("Drivers License")
+weekly_playlist.insert(0,"Bohemian Rhapsody")
+weekly_playlist.remove("Good 4 u")
+print(weekly_playlist.index(1,"Levitating"))
+print(weekly_playlist(3, "As It Was"))
