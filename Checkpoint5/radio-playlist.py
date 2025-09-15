1b54c4e97c71f61c39041d5b9ecc39d8290a26be
Melissa <mizhalpx@gmail.com>
2025-09-14 18:24:14 -0600

    Update radio-playlist.py

--- Checkpoint5/radio-playlist.py
+++ Checkpoint5/radio-playlist.py
@@ -1,11 +1,25 @@
 weekly_playlist = ["Blinding Lights", "Levitating", "As It Was", "Heat Waves", "Good 4 u"]
+print("Weekly paylist: ", weekly_playlist)
 
-weekly_playlist = []
+print("Requested song: Drivers License ")
 weekly_playlist.append("Drivers License")
-weekly_playlist.insert(0,"Bohemian Rhapsody")
+
+print("Song added at the beginning of the playlist: Bohemian Rhapsody ")
+weekly_playlist.insert(0, "Bohemian Rhapsody")
+
+print("Removing this song from the playlist: Good 4 u")
 weekly_playlist.remove("Good 4 u")
+weekly_playlist.index("Levitating")
+weekly_playlist.count("As it was")
+print(weekly_playlist)
 
-commercial_break = weekly_playlist.index("Levitating")
+print("Playlist in reverse: ")
+trowback_thursday = weekly_playlist
+trowback_thursday.reverse()
+print(trowback_thursday)
 
-print("One song of Harry Styles in the list")
-print(weekly_playlist(3, "As It Was"))
+print("Playlist in alphabetical order: ")
+trowback_thursday = weekly_playlist
+trowback_thursday.sort()
+
+print(weekly_playlist)
