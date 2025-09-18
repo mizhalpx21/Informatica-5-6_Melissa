ababc96cb8306d929632c7129326f1d4b687dc79
Melissa <mizhalpx@gmail.com>
2025-09-18 20:01:39 +0000

    h

--- Checkpoint 6/file-io.py
+++ Checkpoint 6/file-io.py
@@ -11,7 +11,16 @@
 # for name in sorted(names):
 #     print(f"Hello {name}")
 
-name = input("Write your name: ")
-file = open("names.txt", "a")
-file.write(f"{name}\n")
-file.close()
+# name = input("Write your name: ")
+# file = open("names.txt", "a")
+# file.write(f"{name}\n")
+# file.close()
+
+# with open("names.txt", "a") as file:
+#     file.write(f"{input("What is your name?")}")
+
+with open("names.txt", "r") as file:
+    lines = file.readlines()         #read each line to make a list
+
+for line in lines: 
+    print(f"Hello{line.rstrip()}")   #adds a hello in each line
