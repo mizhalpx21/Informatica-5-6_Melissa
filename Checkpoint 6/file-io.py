68ba558176554bab3ea6cf3f9477c4cb31762fc0
Melissa <mizhalpx@gmail.com>
2025-09-18 17:29:38 +0000

    1

---
+++ Checkpoint 6/file-io.py
@@ -0,0 +1,17 @@
+# i = input 
+# o = output
+# names = []
+
+# for i in range(3):  #i = position of the list
+#     #name = input("Write your name: ")
+#     # print(f"{i}.Hello {name}")
+#     #names.append(name)
+#     names.append(input("Write your name: "))
+
+# for name in sorted(names):
+#     print(f"Hello {name}")
+
+name = input("Write your name: ")
+file = open("names.txt", "a")
+file.write(f"{name}\n")
+file.close()
