18bbd5697f62767615d3de446d7b8a9321e3305d
Melissa <mizhalpx@gmail.com>
2025-09-22 19:39:24 +0000

    8

---
+++ more_list.py
@@ -0,0 +1,30 @@
+# List with strings
+# names = ["Bob", "Alex", "Kevin"]
+# names.append("Joseph")
+
+# for name in sorted(names):
+#     print(name)
+
+#List with floats
+# measurements = [-2.5, 1.1, 7.5, 14.6, 21.0, 19.2]
+# mean = sum(measurements) / len(measurements)
+# print("The lenght is: ", mean)
+
+#List with list
+# super_list = [[5,2,3],[4,1],[2,2,5,1]]
+# print(super_list[1][0])
+
+# grades = [["Shakira", 8, "D"],["Melissa", 0, "C"],["Shensi", 10, "A"]]
+# for student in grades:
+#     name = student[0]
+#     grade = student[1]
+#     group = student[2]
+#     print(f"{name} from group {group} got a {grade}.")
+
+
+# Matrices 
+matrix = [[1,2,3,],[4,5,6],[7,8,9]]
+#Print rows
+for row in matrix:
+    print(row)
+    for column in matrix:
