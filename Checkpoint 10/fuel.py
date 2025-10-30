b795b1b657ed44f7f6759261e8c0e98114b4af65
Melissa <mizhalpx@gmail.com>
2025-10-30 16:47:56 +0000

    f

--- Checkpoint 10/fuel.py
+++ Checkpoint 10/fuel.py
@@ -1,14 +1,67 @@
 def main():
-    fuel = input("Enter how much fuel in fractions x/y do you have: ").split("/")
-    print(fuel)
+    fuel()
 
-    num = int(fuel[0])
-    print(num)
-    nem = int(fuel[1])
-    print(nem)
+def fuel():
+    condition = True
+    while condition:
+        try:
+            fraction= input("Enter how much fuel as fraction x/y do you have: ").split("/") 
+            num = int(fuel[0])
+            dem = int(fuel[1])
+            percentage= round((num/dem)*100)
+            if percentage > 100:
+                print("Invalid input. Fuel tank cannot be filled more than 100%.")
+                percentage = "E"
+            elif percentage >= 99:
+                print("F")
+                break #(condition = False)
+            elif percentage <= 1:
+                print("E")
+                break
+            else:
+                print(f"The percent is: {percentage}%")
+                break
+        except (ZeroDivisionError, ImportError):
+            print("Invalid fraction")
+        except ValueError:
+            print("Type a number")
 
-    percent= int(num/nem)*100
-    print(f,percent)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# def main():
+#     fuel = input("Enter how much fuel as fraction x/y do you have: ").split("/")
+#     print(fuel)
+
+#     num = int(fuel[0])
+#     dem = int(fuel[1])
+#     percentage= round((num/dem)*100)
+#     print(f"{percentage}%")
+
+# while True:
+#     if percentage <= 1:
+#         percentage = "E"
+#     elif percentage >= 99:
+#         percentage = "F"
+#     elif percentage > 100:
+#         print("Invalid input")
+#         print(percentage)
+
+
 
 
 main()
