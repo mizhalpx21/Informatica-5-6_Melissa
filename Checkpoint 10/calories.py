64c584c9785105ba4d071db9f83556125e399c14
Melissa <mizhalpx@gmail.com>
2025-10-30 20:20:13 +0000

    t

---
+++ Checkpoint 10/calories.py
@@ -0,0 +1,31 @@
+def main():
+    dictionary_food = {
+        "Almond milk" = 30,
+        "Whole" = 73,
+        "Sour Cream, cultured" = 26,
+        "Plain, low-fat" = 77,
+        "egg" = 75,
+        "egg substitute" = 30,
+        "Parmesan Cheese, grated" = 22,
+        "Cottage Cheese, low fat"= 20,
+        "Peanuts, roasted" = 166,
+        "Sunflower seeds, dry roasted" = 165,
+        "Edamame, boiled" = 127,
+        "Lentils, boiled" = 115,
+        "Shrimp, boiled" = 28,
+        "Swai, bake" = 89,
+        "tomato" = 8,
+        "Broccoli" = 7,
+        "Watermellon" = 11,
+        "Peaches" = 15,
+        "Oatmeal, instant" = 77,
+        "Ranch" = 73
+    }
+
+print(f"Available food: {dictionary_food}")
+
+
+
+main()
+
+
