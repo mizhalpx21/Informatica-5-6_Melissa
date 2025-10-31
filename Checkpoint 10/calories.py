0b498187cbc508f7377c73815a1712fc7e3cfb1d
Melissa <mizhalpx@gmail.com>
2025-10-31 17:28:38 +0000

    g

--- Checkpoint 10/calories.py
+++ Checkpoint 10/calories.py
@@ -1,28 +1,40 @@
 def main():
     dictionary_food = {
-        "Almond milk" = 30,
-        "Whole" = 73,
-        "Sour Cream, cultured" = 26,
-        "Plain, low-fat" = 77,
-        "egg" = 75,
-        "egg substitute" = 30,
-        "Parmesan Cheese, grated" = 22,
-        "Cottage Cheese, low fat"= 20,
-        "Peanuts, roasted" = 166,
-        "Sunflower seeds, dry roasted" = 165,
-        "Edamame, boiled" = 127,
-        "Lentils, boiled" = 115,
-        "Shrimp, boiled" = 28,
-        "Swai, bake" = 89,
-        "tomato" = 8,
-        "Broccoli" = 7,
-        "Watermellon" = 11,
-        "Peaches" = 15,
-        "Oatmeal, instant" = 77,
-        "Ranch" = 73
+        "Almond milk": 30,
+        "Whole": 73,
+        "Sour Cream, cultured": 26,
+        "Plain, low-fat": 77,
+        "egg": 75,
+        "egg substitute": 30,
+        "Parmesan Cheese, grated": 22,
+        "Cottage Cheese, low fat": 20,
+        "Peanuts, roasted": 166,
+        "Sunflower seeds, dry roasted": 165,
+        "Edamame, boiled": 127,
+        "Lentils, boiled": 115,
+        "Shrimp, boiled": 28,
+        "Swai, bake": 89,
+        "tomato": 8,
+        "Broccoli": 7,
+        "Watermellon": 11,
+        "Peaches": 15,
+        "Oatmeal, instant": 77,
+        "Ranch": 73
     }
 
-print(f"Available food: {dictionary_food}")
+    clean_list = "\n".join(dictionary_food)
+    print(f"Todays menu: {clean_list}")
+    food = input("Type the name of one food: ")
+    for f in food: 
+        calories = int(input(f"Number of calories of [f]"))
+
+
+
+
+
+   
+
+
 
 
 
