8f736db614ffe042e6566cc44792b2aa0570667a
Melissa <mizhalpx@gmail.com>
2025-11-01 07:00:30 +0000

    y

--- Checkpoint 10/calories.py
+++ Checkpoint 10/calories.py
@@ -1,43 +1,54 @@
-def main():
-    dictionary_food = {
-        "Almond milk": 30,
-        "Whole": 73,
-        "Sour Cream, cultured": 26,
-        "Plain, low-fat": 77,
+dictionary_food = {
+        "almond milk": 30,
+        "milk": 73,
+        "sour Cream, cultured": 26,
+        "plain, low-fat": 77,
         "egg": 75,
         "egg substitute": 30,
-        "Parmesan Cheese, grated": 22,
-        "Cottage Cheese, low fat": 20,
-        "Peanuts, roasted": 166,
-        "Sunflower seeds, dry roasted": 165,
-        "Edamame, boiled": 127,
-        "Lentils, boiled": 115,
-        "Shrimp, boiled": 28,
-        "Swai, bake": 89,
+        "parmesan Cheese, grated": 22,
+        "cottage Cheese, low fat": 20,
+        "peanuts, roasted": 166,
+        "sunflower seeds, dry roasted": 165,
+        "edamame, boiled": 127,
+        "lentils, boiled": 115,
+        "shrimp, boiled": 28,
+        "swai, bake": 89,
         "tomato": 8,
-        "Broccoli": 7,
-        "Watermellon": 11,
-        "Peaches": 15,
-        "Oatmeal, instant": 77,
-        "Ranch": 73
+        "broccoli": 7,
+        "watermelon": 11,
+        "peaches": 15,
+        "oatmeal, instant": 77,
+        "ranch": 73
     }
+
+def main():
 
     clean_list = "\n".join(dictionary_food)
     print(f"Todays menu: {clean_list}")
-    food = input("Type the name of one food: ")
-    for f in food: 
-        calories = int(input(f"Number of calories of [f]"))
 
+    for i in dictionary_food:
+        print(i)
 
+    food = input("Type the name of a food: ").lower()
+    while food not in dictionary_food:
+        food = input("Not in the menu. Try again: ").lower()
+     
+    food2 = input("Type the name of a food: ").lower()
+    while food2 not in dictionary_food:
+        food2 = input("Not in the menu. Try again: ").lower()
 
-
-
-   
-
-
+    if {food, food2} == {"watermelon", "milk"}:
+        print("DOO NOT COMBINE THOSE FOODS")
+    else:
+        print(f"The total calories of {food} and {food2} is {dictionary_food[food] + dictionary_food[food2]} calories.")
 
 
 
 main()
+
+
+
+
+
 
 
