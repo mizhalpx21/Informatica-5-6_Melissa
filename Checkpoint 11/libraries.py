a2be899d8026ebf4bdbfed24e4db77de77240a68
Melissa <mizhalpx@gmail.com>
2025-11-03 19:21:52 +0000

    h

---
+++ Checkpoint 11/libraries.py
@@ -0,0 +1,19 @@
+# import random
+
+# coin = random.choice(["heads", "tails"])     #Chooses between a list
+# print(coin)
+
+# number = random.randint(1,10)
+# print(number)
+
+# cards = ["jacks", "queen", "king", "Ace"]
+# random.shuffle(cards)                   #Varajea al azar
+# for card in cards:
+#     print(card)
+
+# import statistics
+# print(statistics.mean([100,90])) #Prints the mean of 100 plus 90
+# print(statistics)
+
+import sys
+print("Hello, my name is", sys.argv[1])
