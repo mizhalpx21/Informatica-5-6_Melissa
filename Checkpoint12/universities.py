f888a8041e775f14552b2552e09b35a055ee21f9
Melissa <mizhalpx@gmail.com>
2025-11-10 19:36:45 +0000

    i

---
+++ Checkpoint12/universities.py
@@ -0,0 +1,14 @@
+universities = {
+   "URN" = {"carrers": 3,
+        "cost" : 2755,
+        "closest campus" : "Nuevo Casas Grandes"},
+    "UACJ" = {"carrers": 8,
+            "cost" : 3800,
+            "closest campus" : "Nuevo Casas Grandes"},
+    "La SALLE" = {
+        "carrers" : 19,
+        "cost" : 60000,
+        "closest campus" : "Chihuahua"},
+    "UT Paquime" =
+    
+}
