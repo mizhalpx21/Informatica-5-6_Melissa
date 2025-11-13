549a2db2f1f1f6606569cee3a624ced8fd70fa3e
Melissa <mizhalpx@gmail.com>
2025-11-13 16:24:59 +0000

    m

--- Checkpoint12/bitcoin.py
+++ Checkpoint12/bitcoin.py
@@ -4,4 +4,10 @@ API_KEY = "27be317712d2e0375eddb213e8dc4cf2fbbbb05662302b9b819757b6f9cb4e8b"
 
 bitcoin = float(input("How many bitccoins do you want to buy? "))
 
-response = requests.get("https://rest.coincap.o/v3/assets/bitcoin?apiKey="+API_KEY)
+response = requests.get("https://rest.coincap.io/v3/assets/bitcoin?apiKey="+API_KEY)
+#print(response.json())
+bitcoin_price = float(response.json()["data"]["priceUsd"])
+
+
+total = round(bitcoin * bitcoin_price, 2)
+print(f"Your total will be: ${total:,.2f}")
