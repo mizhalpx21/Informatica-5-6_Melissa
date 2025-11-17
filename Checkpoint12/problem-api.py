8b081ae1b7bc107b4e5ef471e4345e588ecfc66f
Melissa <mizhalpx@gmail.com>
2025-11-17 02:34:00 +0000

    t

--- Checkpoint12/problem-api.py
+++ Checkpoint12/problem-api.py
@@ -1,2 +1,25 @@
 import requests
+print("Welcome to our makeup API, we are going to help you find the perfect makeup for you from the brand Mabelling!!!")
+skin_tones = [
+    "Type I: Light,pale white", 
+    "Type II: White, fair",
+    "Type III: Medium, white to olive",
+    "Type IV: Olive, moderate brown",
+    "Type V: Brown, dark brown",
+    "Type VI: Brown, very dark, brown to black"
+]
+clean_list = "\n".join(skin_tones)
+print(f"Skin tones:{clean_list}")
+tone = input("Selct your type of skintone: ")
+ 
+product = [
+    "eyeshadow",
+    "lipstick",
+    "eyeliner",
+    "mascara",
+    "lip_liner",
+    "bronzer",
+    "blush",
+    "foundation"
+]
 
